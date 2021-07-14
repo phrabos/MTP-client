36760c1534f78442d88fee7214e99e3289ad3a5c
patrick hrabos <phrabos@gmail.com>
2021-07-14 14:55:37 -0400

    fix type interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { deleteTea, getAllTeas } from './utils/apiUtils';
+import { deleteTea, getAllTeas, updateTea } from './utils/apiUtils';
 
 const App: React.FC = () => {
 	return (
@@ -6,6 +6,11 @@ const App: React.FC = () => {
 			<h1>working</h1>
 			<button onClick={() => getAllTeas().then(console.log)}>get all</button>
 			<button onClick={() => deleteTea(1).then(console.log)}>delete</button>
+			<button
+				onClick={() => updateTea(2, { teaType: 'dark tea' }).then(console.log)}
+			>
+				update
+			</button>
 		</div>
 	);
 };
--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,14 +1,14 @@
 interface Tea {
-	id: number;
-	name: string;
-	cultivar: string;
-	elevation: number;
-	harvestYear: number;
-	inStock: boolean;
-	origin: string;
-	quantity: number;
-	teaType: string;
-	vendorName: string;
+	id?: number;
+	name?: string;
+	cultivar?: string;
+	elevation?: number;
+	harvestYear?: number;
+	inStock?: boolean;
+	origin?: string;
+	quantity?: number;
+	teaType?: string;
+	vendorName?: string;
 }
 
 const DEV = 'http://localhost:8000';
@@ -20,11 +20,26 @@ export async function getAllTeas(): Promise<Tea[]> {
 	return json;
 }
 
-export async function deleteTea(id: number): Promise<Tea[] | string> {
+export async function deleteTea(
+	id: number
+): Promise<Tea[] | { message: string }> {
 	const data = await fetch(`${DEV}/teas/${id}`, {
 		method: 'DELETE',
 	});
 	const json = await data.json();
-	console.log(json);
+
+	return json;
+}
+
+export async function updateTea(id: number, dataObj: Tea): Promise<Tea> {
+	const data = await fetch(`${DEV}/teas/${id}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(dataObj),
+	});
+	const json = await data.json();
+
 	return json;
 }
