d3a94e825f2ac261b272a53134bd482a12269176
patrick hrabos <phrabos@gmail.com>
2021-08-31 11:49:16 -0700

    add dynamic sign in or create account form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../components/nav';
 
 const Login: React.FC = () => {
+	const [isLogin, setIsLogIn] = useState(true);
+
 	const handleSubmit = (event: React.FormEvent): void => {
 		event.preventDefault();
 		console.log('sign in clicked');
 	};
 
+	const handleAuthMode = () => {
+		setIsLogIn((prevState) => !prevState);
+	};
+
 	return (
 		<>
 			<Nav />
+			<h3>{isLogin ? 'Sign In' : 'Sign Up'}</h3>
 			<form onSubmit={handleSubmit}>
 				<input type="text" placeholder="username" required></input>
 				<input type="password" placeholder="password" required></input>
-				<button>Sign in</button>
+				<button>{isLogin ? 'Sign In' : 'Create Account'}</button>
+				<p style={{ cursor: 'pointer' }} onClick={handleAuthMode}>
+					{isLogin ? 'Create new account' : 'Login with existing account'}
+				</p>
 			</form>
 		</>
 	);
