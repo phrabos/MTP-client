e06767f257b2730f524408b24ba7c259b7d57d06
patrick hrabos <phrabos@gmail.com>
2021-08-31 13:46:29 -0700

    add fetch to proxy server to signup via Firebase Auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Nav from '../components/nav';
 
 const Login: React.FC = () => {
 	const [isLogin, setIsLogIn] = useState(true);
+	const emailInputRef = useRef<HTMLInputElement>(null);
+	const passwordInputRef = useRef<HTMLInputElement>(null);
 
 	const handleSubmit = (event: React.FormEvent): void => {
 		event.preventDefault();
-		console.log('sign in clicked');
+		const email = emailInputRef.current?.value;
+		const password = passwordInputRef.current?.value;
+		console.log(email, password);
+		if (isLogin) {
+		} else {
+			fetch('http://localhost:8000/account/signup', {
+				method: 'POST',
+				body: JSON.stringify({
+					email,
+					password,
+				}),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then((res) => res.json())
+				.then((data) => console.log(data));
+		}
+		// history.pushState()
 	};
 
-	const handleAuthMode = () => {
+	const handleAuthMode = (): void => {
 		setIsLogIn((prevState) => !prevState);
 	};
 
@@ -18,8 +38,18 @@ const Login: React.FC = () => {
 			<Nav />
 			<h3>{isLogin ? 'Sign In' : 'Sign Up'}</h3>
 			<form onSubmit={handleSubmit}>
-				<input type="text" placeholder="username" required></input>
-				<input type="password" placeholder="password" required></input>
+				<input
+					ref={emailInputRef}
+					type="text"
+					placeholder="email"
+					required
+				></input>
+				<input
+					ref={passwordInputRef}
+					type="password"
+					placeholder="password"
+					required
+				></input>
 				<button>{isLogin ? 'Sign In' : 'Create Account'}</button>
 				<p style={{ cursor: 'pointer' }} onClick={handleAuthMode}>
 					{isLogin ? 'Create new account' : 'Login with existing account'}
